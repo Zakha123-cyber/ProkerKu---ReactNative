54182dcfc74f2421dc05a2374aa6eefa3d9b688c
Zakha123-cyber <aguscow6@gmail.com>
2024-11-13 00:07:58 +0700

    tambah asset dan constant

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./component/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00FF9C",
+      },
+    },
   },
   plugins: [],
 };
