bef2d1f0d3918a490669972199d96b78090850dc
deviesAde <deviesade@gmail.com>
2024-12-11 02:10:56 +0700

    update home component to use HomeAnggotaNavigator

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import HomeKetumNavigator from "../dashboard/ketum/HomeKetumNavigator";
-
-
+import HomeKetuaProkerNavigator from "../dashboard/ketuaPro/HomeKetuaProNavigator";
+import HomeCoDivisiNavigator from "../dashboard/co/HomeCoDivisiNavigator";
+import HomeAnggotaNavigator from "../dashboard/anggota/HomeAnggotaNavigator";
 
 const home = () => {
-  return <HomeKetuaProkerNavigator />;
+  return <HomeAnggotaNavigator />;
 };
 
 export default home;
